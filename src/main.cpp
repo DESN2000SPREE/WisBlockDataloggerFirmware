4b828d5c08a5f77cc3611dea4f45eebe9799e70f
DESN2000SPREE <114446320+DESN2000SPREE@users.noreply.github.com>
2022-09-27 14:46:06 +1000

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Place your main.cpp code here to compile and load to the device.
+ * Once you're ready I would recommend copying the combined_lib_example.cpp here:
+ * \examples\Combined_lib_example\
+ */
+
+#include <Arduino.h>
+
+/**
+ * @brief Setup code runs once on reset/startup.
+ */
+void setup() {}
+
+/**
+ * @brief Loop code runs repeated after setup().
+ */
+void loop() {}
